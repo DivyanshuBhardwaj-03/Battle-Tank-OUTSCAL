941673504fbaf92fa5d63bd228dea6542fae3452
DivyanshuBhardwaj-03 <divyanshubhardwaj013@gmail.com>
2022-04-25 21:38:38 +0530

    6th Commit ( UI Interface)
    Implemented UI Interface, allowing player to chose different colored tanks with each having different properties.

--- Assets/Scripts/TankSelection.cs
+++ Assets/Scripts/TankSelection.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class TankSelection : MonoBehaviour
 {
 
-    public TankSpawner tankSpawner;
+   public TankSpawner tankSpawner;
    public void BlueTankSelected()
-       {
-
-    }
+   {
+        tankSpawner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+   }
 
     public void GreenTankSelected()
     {
-
+        tankSpawner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
     }
 
     public void RedTankSelected()
     {
-
+        tankSpawner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
     }
 }
